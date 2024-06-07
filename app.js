b7e667de15efc1bd385f94b719a1b664cd7e9988
rakesh025-hub <108419721+rakesh025-hub@users.noreply.github.com>
2024-06-07 12:49:24 +0530

    fully working

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
-var randomnum = Math.floor(Math.random() * 6)+ 1;
-   
+var imagearray = [
+    "images/dice1.png",
+    "images/dice2.png",
+    "images/dice3.png",
+    "images/dice4.png",
+    "images/dice5.png",
+    "images/dice6.png",
+];
+var randomnum1 = Math.floor(Math.random() * imagearray.length);
+var randomnum2 = Math.floor(Math.random() * imagearray.length);
 
-var randimag = randnuu();
-var image11 = document.getElementsByClassName("image1");
-image11.setAttribute("src", randimag);
+if (randomnum1 > randomnum2) {
+    document.getElementById("winner").innerHTML = "player1 wins"
+} 
+else if (randomnum1 < randomnum2) {
+    document.getElementById("winner").innerHTML = "player2 wins"
+
+}
+else {
+    document.getElementById("winner").innerHTML = "draw"
+}
+
+var imagesource1 = imagearray[randomnum1];
+var imagesource2 = imagearray[randomnum2];
+
+var image1 = document.getElementById("image1").setAttribute("src", imagesource1);
+var image2 = document.getElementById("image2").setAttribute("src", imagesource2);
